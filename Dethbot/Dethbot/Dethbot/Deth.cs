3d1911672460168f1bfec4e970d6ec95fe1ff2b0
Thomas Beswick <bestho@nith.no>
2014-02-25 00:53:50 +0100

    panic

--- Dethbot/Dethbot/Dethbot/Deth.cs
+++ Dethbot/Dethbot/Dethbot/Deth.cs
@@ -8,6 +8,8 @@ namespace DarkCodex
 {
     class Deth : Robot
     {
+        private int panic = 0;
+
         public override void Run()
         {
             Start();
@@ -26,13 +28,29 @@ namespace DarkCodex
         // Perform robot logic here calling robot commands etc.
         private void Update()
         {
-            TurnGunLeft(5);
+            if (panic == 0)
+            {
+                TurnGunLeft(5);
+            }
+            else
+            {
+                TurnGunLeft(20);
+                panic--;
+            }
         }
 
         //When the robot sees another
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
             Fire(1);
+            Fire(1);
+            Fire(1);
+            Fire(10);
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            panic = 20;
         }
     }
 }
