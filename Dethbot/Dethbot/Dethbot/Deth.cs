913c7d35a5b2e10d07f1472612f5cef82f158869
Thomas Beswick <bestho@nith.no>
2014-02-25 00:17:18 +0100

    lol

---
+++ Dethbot/Dethbot/Dethbot/Deth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Robocode;
+
+namespace DarkCodex
+{
+    class Deth : Robot
+    {
+        public override void Run()
+        {
+            Start();
+
+            while (true)
+            {
+                Update();
+            }
+        }
+
+        // Perform your initialization for your robot here
+        private void Start() {
+
+        }
+
+        // Perform robot logic here calling robot commands etc.
+        private void Update()
+        {
+            TurnGunLeft(5);
+        }
+
+        //When the robot sees another
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            Fire(1);
+        }
+    }
+}
